52430edbac719901c873a90eacf23f597275681a
charles-afk <agudelo.charles@gmail.com>
2022-10-16 22:57:19 -0400

    added tut 1

--- js/js1.js
+++ js/js1.js
@@ -1,7 +1,7 @@
 let app1 = angular.module('app1', []);
 app1.controller('ctrl1', ($scope) => {
     $scope.first = 1;
-    $scope.second = 1;
+    $scope.second = 2;
 
     $scope.updateValue = () => {
         $scope.calculation = $scope.first + ' + ' + $scope.second + ' = ' + (+$scope.first + +$scope.second);
